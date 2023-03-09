2f299de4c74d99343127f96e827d07b09e413be5
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-03-09 18:45:15 -0500

    feature: buttons & button overlay

--- app/views/browseView.js
+++ app/views/browseView.js
@@ -1,11 +1,20 @@
-const renderQuote = (quote) => `  
-    <figure class="card">
-      <img src="./images/quotes/quote${quote.id}.jpg" alt="${quote.author}" />
+const renderQuote = (quote) => `
+    <div class="card-holder">  
+      <figure class="card">
+        <div class="image-holder">
+          <img src="./images/quotes/quote${quote.id}.jpg" alt="${quote.author}" />
+          <button class="btn btn--xsmall btn--round btn-overlay">View</button>
+        </div>
         <figcaption class="card-body">
           <h1 class="card-title">${quote.author}</h1>
           ${quote.text}          
         </figcaption>
-    </figure>
+      </figure>
+      <div class="button-holder">
+        <button class="btn btn--xsmall btn--round">Edit</button>
+        <button class="btn btn--xsmall btn--round">Delete</button>        
+      </div>
+    </div>
 `;
 
 const renderQuotes = (quotes) => `
